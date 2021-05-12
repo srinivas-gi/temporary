f44c9592d5239b2f83223bc5553db732e95e8e98
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2021-05-12 20:49:51 +0600

    Check download link

--- build_rom.sh
+++ build_rom.sh
@@ -9,5 +9,4 @@ lunch derp_vayu-userdebug
 mka derp
 
 # upload rom
-device=$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1)
-rclone copy out/target/product/vayu/DerpFest*.zip cirrus:"$device" -P
+rclone copy out/target/product/vayu/DerpFest*.zip cirrus:vayu -P
