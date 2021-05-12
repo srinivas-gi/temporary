14920721d4159b13a34078959a5e714dae759ac3
rahul9999xda <rahul9999xda@gmail.com>
2021-05-12 07:57:15 +0000

    Regenerate Script accoring to new Template
    Signed-off-by: rahul9999xda <rahul9999xda@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,7 @@
-#!/bin/bash
-
-set -e
-set -x
-
 # sync rom
-repo init -u git://github.com/DerpFest-11/manifest.git -b 11 --depth=1
+repo init --depth=1 -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/SuperCosmicBeing/frostmanifest.git --depth=1 -b derp .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #Setup
 curl https://gist.githubusercontent.com/SuperCosmicBeing/c6db35d8abfa480e183a66b71ffca563/raw/3f5f1fc6d02ae767479bda37c04ea417a229f7eb/gms_full.mk >> gms_full.mk
@@ -19,9 +14,5 @@ lunch derp_sakura-user
 mka derp
 
 # upload rom
-up(){
-	curl --upload-file $1 https://transfer.sh/$(basename $1); echo
-	# 14 days, 10 GB limit
-}
-
-up out/target/product/sakura/*.zip
+device=$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1)
+rclone copy out/target/product/sakura/DerpFest*.zip cirrus:$device -P
