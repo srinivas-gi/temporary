d069c573e459eddef00deb868bc9ce8dd208985c
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2021-05-12 15:46:02 +0600

    try fixing sync

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-
 # sync rom
 repo init -u git://github.com/crdroidandroid/android.git --depth=1 -b 11.0 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/boedhack/local_manifest.git -b 11.0 --depth 1 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) -f
 
 # build rom
 source build/envsetup.sh
